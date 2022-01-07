a688ed37839d43e2a855d7a5b6e25003ca57956f
Gitte Vermeiren <gitte.vermeiren@gmail.com>
2022-01-07 15:19:02 +0100

    Update install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -30,16 +30,16 @@
     rm -f helm.tar.gz
     
     # download azdo agent
-    mkdir -p /opt/azdo && cd /opt/azdo
-    cd /opt/azdo
-    curl -o azdoagent.tar.gz https://vstsagentpackage.azureedge.net/agent/2.195.2/vsts-agent-linux-x64-2.195.2.tar.gz
-    tar xzvf azdoagent.tar.gz
-    rm -f azdoagent.tar.gz
+    #mkdir -p /opt/azdo && cd /opt/azdo
+    #cd /opt/azdo
+    #curl -o azdoagent.tar.gz https://vstsagentpackage.azureedge.net/agent/2.195.2/vsts-agent-linux-x64-2.195.2.tar.gz
+    #tar xzvf azdoagent.tar.gz
+    #rm -f azdoagent.tar.gz
     
     # configure as azdouser
-    chown -R $agentuser /opt/azdo
-    chmod -R 755 /opt/azdo
-    runuser -l $agentuser -c "/opt/azdo/config.sh --unattended --url $azdourl --auth pat --token $pat --pool $pool --acceptTeeEula"
+    #chown -R $agentuser /opt/azdo
+    #chmod -R 755 /opt/azdo
+    #runuser -l $agentuser -c "/opt/azdo/config.sh --unattended --url $azdourl --auth pat --token $pat --pool $pool --acceptTeeEula"
     
     # start the service
-    ./run.sh
+    #./run.sh
